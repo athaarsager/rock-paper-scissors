a8bbd1bc7b8aff4631a211b86c3b9ed51703b46c
Andrew Haarsager <athaarsager@gmail.com>
2023-10-28 18:48:11 -0500

    Add pseudocode to javascript.js

---
+++ javascript.js
@@ -0,0 +1,16 @@
+/* Main goal: Make a program that lets the user play rock paper scissors
+against the computer.
+
+Create a variable called getComputerChoice that will randomly
+return either rock, paper, or scissors. */
+
+/* Write a function that plays a single round of rock paper scissors.
+The function should take two parameters-the playerSelection and 
+computerSelection. Return a string that declares the winner
+"You lose! Paper beats Rock" etc.
+--make parameter case-insensitive */
+
+/*Write a new function called game(). Use the previous function
+inside of this one to play a 5-round game that keeps a score and
+reports a winner or loser at the end.
+*/
