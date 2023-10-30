5d83c829a1b3b0ac09649caa9391df91f5f394f8
Andrew Haarsager <athaarsager@gmail.com>
2023-10-30 09:08:05 -0500

    Remove pseud-code for cleaner look

--- javascript.js
+++ javascript.js
@@ -1,12 +1,3 @@
-/* Main goal: Make a program that lets the user play rock paper scissors
-against the computer.
-
-Create a function called getComputerChoice that will randomly
-return either rock, paper, or scissors. Declare the variable, 
-then create function that uses Math.Random() *10 plus math.floor
-to generate a number "i" between 0 and 9. If 0, re-generate, if 1-3, pick rock, if 3-5 pick paper,
-if 6-9, pick scissors. return the output to getComputerChoice*/
-
 let playerScore = 0;
 let computerScore = 0;
 
@@ -26,17 +17,6 @@ function getComputerChoice() {
    }
 }
 
-
-/* Write a function that plays a single round of rock paper scissors.
-The function should take two parameters-the playerSelection and 
-computerSelection. 
-Create variable playerSelection that prompts the user to enter rock, paper, or scissors.
-set playerSelection to lowercase. Create for loop to display error message
-if player does not enter rock, paper, or scissors.
-Set computerSelection equal to the output of getComputerChoice.
-Return a string that declares the winner by using an if else or swith for the
-various win/lose possibilities.
-"You lose! Paper beats Rock" etc.*/
     
 function getPlayerChoice() {
     let answer = prompt("Rock, Paper, or Scissors?");
@@ -47,8 +27,6 @@ function getPlayerChoice() {
     };
     return answer;
 }
-
-
 
 function playRound(playerSelection, computerSelection) {
     if(playerSelection === computerSelection) {
@@ -79,16 +57,6 @@ function playRound(playerSelection, computerSelection) {
         return "Rock beats scissors. You lose :(";
     }
 }
-
-/*Write a new function called game(). Use the previous function
-inside of this one to play a 5-round game that keeps a score and
-reports a winner or loser at the end.
-Create variable roundNumber and set it equal to 1.
-In game(), create while loop that runs until roundNumber = 5.
-Console.log the results of each round and the current roundNumber.
-After round 5, Console.log the winner.
-Create if statement to account for 5 ties in a row.
-*/
 
 function game() {
     for(let roundNumber =1; roundNumber < 6; roundNumber++) {
